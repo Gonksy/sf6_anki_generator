ee85514359e1e7db2968e9cf283e5de49d799fdb
Ahmad Yosry <87082240+Gonksy@users.noreply.github.com>
2024-04-23 23:12:12 +0200

    Moved processes to helpers.js

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-const XLSX = require("xlsx")
-const workbook = XLSX.readFile("./sf6Data.xlsx")
-const util = require("util")
+const { processSheets } = require("./helpers")
 
-const { SheetNames } = workbook
+processSheets()()
 
-const normalSheets = SheetNames.filter((sheetName) =>
-  sheetName.includes("Normal")
-).reduce((obj, currentSheetName) => {
-  const currentSheet = XLSX.utils.sheet_to_json(
-    workbook.Sheets[currentSheetName]
-  )
-  obj[currentSheetName] = currentSheet
-  return obj
-}, {})
-
-const { RyuNormal } = normalSheets
-RyuNormal.forEach((move) => {
-  console.log(`How plus/minus is ${move.moveName} on block?`)
-  console.log(`${move.moveName} is ${move.onBlock} on block`)
-})
+// RyuNormal.forEach((move) => {
+//   console.log(`How plus/minus is ${move.moveName} on block?`)
+//   console.log(`${move.moveName} is ${move.onBlock} on block`)
+// })
 
 // console.log(normalSheetNames)

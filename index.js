ae835565fe5c5f0d9eadba1eb7971f9143fda51e
Ahmad Yosry <87082240+Gonksy@users.noreply.github.com>
2024-04-29 12:27:02 +0300

    Fixed accessing sheets after generation

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const { processSheets } = require("./helpers")
 const fs = require("fs")
 const util = require("util")
 const path = "./sheets/sheets.json"
-const sheets = fs.existsSync(path) ? require(path) : undefined
+let sheets = fs.existsSync(path) ? require(path) : undefined
 
 if (!sheets) {
   console.log(`Sheets not found. Generating sheets from local sf6Data.xlsx`)
-  processSheets()()
+  sheets = processSheets()()
 } else {
   console.log(`Existing sheets loaded successfully`)
 }
 
-console.log(util.inspect(sheets["Ryu"].normals, { depth: 1 }))
+const guile = sheets.guile
+console.log(util.inspect(guile, { depth: 0 }))
